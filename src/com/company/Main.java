e87a0232135cbe649079a6a010c3459e6999432a
SHUBHAM RAJ <hey.shubhamraj@gmail.com>
2021-08-17 11:10:10 +0530

    For loop Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args){
+        float f=10L;
+        int[] arr = {1,23,45,67,89,567,34,234,123,};
+        Arrays.sort(arr);
+//        Arrays.binarySearch(arr, 89);
+        for (int i = 0; i < arr.length-1; i++) {
+            System.out.println(arr[i]);
+        }
 
     }
 }
