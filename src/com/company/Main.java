d57be447b8e43b0e40918a3fde8acdae8f545daf
SHUBHAM RAJ <hey.shubhamraj@gmail.com>
2021-08-17 11:18:27 +0530

    MinMax Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,12 +6,21 @@ public class Main {
 
     public static void main(String[] args){
         float f=10L;
-        int[] arr = {1,23,45,67,89,567,34,234,123,};
+        int[] arr = {1,23,45,67,89,567,34,234,123};
         Arrays.sort(arr);
 //        Arrays.binarySearch(arr, 89);
-        for (int i = 0; i < arr.length-1; i++) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
+        MinMAx(arr);
 
     }
+
+    public static int MinMAx (int[] arr){
+        int n = arr.length -1;
+        System.out.println("Minimum Element of an Array : "+arr[0]);
+        System.out.println("Maximum Element of an Array : "+arr[n]);
+        return 0;
+    }
+
 }
